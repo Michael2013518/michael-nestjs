3949c89a7760afadd0b0023fd3453a15dced8e47
michael <zdong2010@gmail.com>
2019-10-30 08:32:58 +0800

    add modules and update to use interface and service

---
+++ src/modules/posts/posts.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Req, Query, Headers, Param, Post, Body } from '@nestjs/common';
+import { CreatePostDto } from './post.dto';
+import { DemoService } from './providers/demo/demo.service';
+
+@Controller('posts')
+export class PostsController {
+    constructor( private readonly demoService: DemoService) {
+        this.demoService = demoService;
+    }
+    @Get()
+    index() {
+      return this.demoService.findAll();
+    }
+    @Get(':id')
+    show(@Param() params) {
+        return {
+            title: `posts ${params.id}`;
+        }
+    }
+    @Post()
+    store(@Body() post: CreatePostDto) {
+      this.demoService.create(post);
+    }
+}
